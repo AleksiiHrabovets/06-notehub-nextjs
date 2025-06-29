3bb85caeea3b2ed4b01dfe829e4a0b8f8a3788df
ALeksiiHrabovets <hrabovets78@gmail.com>
2025-06-29 19:59:03 +0300

    patch07

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -2,7 +2,11 @@ import { fetchNotes } from '@/lib/api';
 import NotesClient from './Notes.client';
 
 export default async function NotesPage() {
-  const { notes, totalPages } = await fetchNotes('', 1); // initial load
-
-  return <NotesClient initialNotes={notes} initialTotalPages={totalPages} />;
+  try {
+    const { notes, totalPages } = await fetchNotes('', 1); // initial load
+    return <NotesClient initialNotes={notes} initialTotalPages={totalPages} />;
+  } catch (error) {
+    console.error('Error loading notes:', error);
+    return <div>❌ Не вдалося завантажити нотатки. Спробуйте пізніше.</div>;
+  }
 }
